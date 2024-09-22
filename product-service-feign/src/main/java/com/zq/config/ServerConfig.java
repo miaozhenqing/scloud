8cb491eab513c6fb31175ab4478578c8884b9ece
miaozhenqing <mzqingmail@163.com>
2024-09-22 20:51:37 +0800

    配置客户端测试修改

---
+++ product-service-feign/src/main/java/com/zq/config/ServerConfig.java
@@ -0,0 +1,14 @@
+package com.zq.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServerConfig {
+    @Value("${version?default}")
+    private String version;
+
+    public String getVersion() {
+        return version;
+    }
+}
--- product-service-feign/src/main/java/com/zq/controller/ProductController.java
+++ product-service-feign/src/main/java/com/zq/controller/ProductController.java
@@ -2,9 +2,11 @@ package com.zq.controller;
 
 import java.util.List;
 
+import com.zq.config.ServerConfig;
 import com.zq.pojo.Product;
 import com.zq.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +17,12 @@ public class ProductController {
 
     @Autowired
     ProductService productService;
-
+    @Autowired
+    ServerConfig serverConfig;
     @RequestMapping("/products")
     public Object products(Model m) {
         List<Product> ps = productService.listProducts();
+        m.addAttribute("version", serverConfig.getVersion());
         m.addAttribute("ps", ps);
         return "products";
     }
